dd9a3c330182d9f48b540979ac2164b531ffa46f
Giulio <3272563+giuliohome@users.noreply.github.com>
2024-03-25 22:15:34 +0000

    simplified reproducible example
    for https://github.com/SonarSource/sonar-dotnet/issues/8977

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Generated;
+using GeneratedCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        GeneratedClass generator = new GeneratedClass();
+        generator.CreatePackage("output.xlsx");
+    }
+}
